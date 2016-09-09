34bdaea25e87db04b7fb92fcf9e8dea8c4f5510a
Vincent <liwensen@ethank.com.cn>
2016-09-09 11:45:04 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # pin
+将元素定于页面某个位置
+
+## Usage
+依赖于zepto或jQuery，支持AMD规范，页面引入pin.js及pin.css样式
+```
+<link rel="stylesheet" href="css/pin.css"/>
+<script src="pin.js"></script>
+```
+
+待页面加载就绪后执行绑定方法初始化pin
+```javascript
+Pin.bind({
+  target: '#ele'
+});
+```
+
+支持配置offset参数指定据窗口顶部距离
+```javascript
+Pin.bind({
+  target: '#ele',
+  offset: 50
+});
+```
+
+
+target参数支持数组形式，以传入多个元素
+```javascript
+Pin.bind({
+  target: ['#ele_1', '#ele_2', ...]
+});
+```
+
+多个元素同样可以使用对象形式指定offset参数
+```javascript
+Pin.bind({
+  target: [{target: '#ele_1', offset: 40}, {target: '#ele_2', offset: 50}, ...]
+});
+```
+
+如果target元素在交互过程中被重新绘制（常发生于模板局部刷新）。可使用rebind方法重新绑定指定元素
+```javascript
+Pin.rebind({
+  target: '#ele',
+  hackModel: true
+});
+```
---
+++ pin.js
@@ -0,0 +1,185 @@
+define(['zepto'], function($) {
+  'use strict';
+
+  var CONFS = [];
+  var defaults = {
+    target: null,
+    contextEle: document,
+    offset: 0,
+    hackModel: false
+  };
+  var settings = null;
+  var utils = {
+    // 判断是否为微信浏览器
+    isWeixin: function () {
+      return /MicroMessenger/.test(navigator.userAgent);
+    },
+    isArray: function (a) {
+      return Array.isArray ? Array.isArray(a) : Object.prototype.toString.call(a) === '[object Array]';
+    }
+  };
+
+  function init(options) {
+    settings = $.extend(true, {}, defaults, options);
+    initConfig();
+  }
+  
+  /**
+   * 生成配置数据
+   * @param  {String} target 目标元素id
+   * @param  {Number} offset 偏移量
+   * @return {Object}        配置数据
+   */
+  function createConfItem(target, offset) {
+    var $target = $(target);
+    offset = Number.isNaN(Number(offset))?0:offset;
+    var originTop = $target.offset().top - offset;
+    var topCache = $target.css('top');
+    var width = $target.css('width');
+    return {
+      $target: $target,
+      selector: target,
+      offset: offset,
+      width: width,
+      topCache: topCache,
+      originTop: originTop
+    };
+  }
+
+  /**
+   * 多元目标元素配置
+   * @param  {[type]} target 目标元素id
+   * @return {Array}         配置数据
+   */
+  function createMutiConfData(target) {
+    var configs = [];
+    target.forEach(function(item) {
+      if(typeof(item)==='string') {
+        configs.push(createConfItem(item, settings.offset));
+      } else {
+        configs.push(createConfItem(item.target, item.offset));             
+      }
+    });
+    return configs;
+  }
+
+  function contextReady() {
+    var configs = [],
+        target = settings.target;
+    if(utils.isArray(target)) {
+      configs = createMutiConfData(target);
+    } else {
+      configs.push(createConfItem(target, settings.offset));
+    }
+
+    CONFS = configs;
+    console.log(CONFS);
+    bindScrollEvent();
+  }
+
+  /**
+   * 初始化配置
+   * @param  {Function} cb     回调函数
+   */
+  function initConfig() {
+    var context = settings.contextEle || window;
+    context.onload = function() {
+      contextReady();
+    };
+    contextReady();
+  }
+
+  function fixedTop(config) {
+    config.$target.addClass('pin-fixed-top').css({'top': config.offset+'px', 'width': config.width});
+  }
+
+  function removeFixedTop(config) {
+    config.$target.removeClass('pin-fixed-top').css('top', config.topCache);
+  }
+
+  /**
+   * 执行pin操作
+   * @param  {Number} scroll 当前滚动距离
+   * @param  {Object} config 配置数据
+   */
+  function pinIt(scroll, config) {
+    if(scroll>=config.originTop) {
+      fixedTop(config);
+    } else {
+      removeFixedTop(config);
+    }
+  }
+
+  /**
+   * 获取滚动条滚动距离
+   * @return {Number} [description]
+   */
+  function getScrollTop() {
+    return document.documentElement.scrollTop || document.body.scrollTop;
+  }
+
+  /**
+   * 滚动事件处理函数
+   */
+  function scrollHandleFn(event) {
+    event.preventDefault();
+    var scroll = getScrollTop();
+    CONFS.forEach(function(config) {
+      pinIt(scroll, config);
+    });
+  }
+
+  /**
+   * hack微信onscroll实时响应问题
+   */
+  function hackWechat() {
+    if(utils.isWeixin()) {
+      $('window').on('touchmove', scrollHandleFn);
+    } else {
+      window.onscroll = scrollHandleFn;
+    }
+  }
+
+  /**
+   * 绑定scroll事件
+   * @param  {Object} configs 配置数据
+   */
+  function bindScrollEvent() {
+    // hack模式，开启可能会导致性能下降
+    if(settings.hackModel) {
+      hackWechat();
+    } else {
+      window.onscroll = scrollHandleFn;
+    }
+  }
+
+  function rebind(targets) {
+    var configs = [],
+        offset = 0;
+    if(utils.isArray(targets)) {
+      configs = createMutiConfData(targets);
+    } else {
+      offset = targets.offset || 0;
+      configs.push(createConfItem(targets.target, offset));
+    }
+    configs.forEach(function(item) {
+      mixin(item);
+    });
+
+    function mixin(newConf) {
+      for(var i=0, len=CONFS.length; i<len; i++) {
+        if(CONFS[i].selector === newConf.selector) {
+          CONFS[i] = newConf;
+          break;
+        }
+      }
+    }
+
+    console.log(CONFS);
+  }
+
+  return {
+    bind: init,
+    rebind: rebind
+  };
+});
